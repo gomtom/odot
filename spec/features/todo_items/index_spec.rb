efbeeedfd62270855ece31fb0a77cc5eb627ff89
Thomas Harrold <tharrold@internode.on.net>
2016-07-15 11:42:41 +1000

    Generated todo item model, controller and routes. Also added associated specs

---
+++ spec/features/todo_items/index_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Viewing todo items" do 
+	let!(:todo_list) { TodoList.create(title: "Grocery List", description: "Groceries") }
+
+	it "displays no items when a todo list is empty" do
+		visit "/todo_lists"
+	end
+end
