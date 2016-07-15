615039d421589f28b03d1bc1c41d90f7ac97e40e
Thomas Harrold <tharrold@internode.on.net>
2016-07-15 12:50:53 +1000

    Added todo_items controller

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -5,5 +5,10 @@ describe "Viewing todo items" do
 
 	it "displays no items when a todo list is empty" do
 		visit "/todo_lists"
+		within "#todo_list_#{todo_list.id}" do
+			click_link "List Items"
+		end
+
+		expect(page).to have_content("TodoItems#index")
 	end
 end
