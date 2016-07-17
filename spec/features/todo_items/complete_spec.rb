44faf590f28d7a2b6923da97eeae76d20e1988ae
Thomas Harrold <tharrold@internode.on.net>
2016-07-17 23:32:55 +1000

    Finishing touches

--- spec/features/todo_items/complete_spec.rb
+++ spec/features/todo_items/complete_spec.rb
@@ -6,11 +6,29 @@ describe "Completing todo items" do
 
 	it "is successful when marking a single item complete" do
 		expect(todo_item.completed_at).to be_nil
-		visit_todo_list todo_list
+		visit_todo_list(todo_list)
 		within dom_id_for(todo_item) do
 			click_link "Mark Complete"
 		end
 		todo_item.reload
 		expect(todo_item.completed_at).to_not be_nil
+	end
+
+	context "with comleted items" do
+		let!(:completed_todo_item) { todo_list.todo_items.create(content: "Eggs", completed_at: 5.minutes.ago) }
+
+		it "shows the completed items as complete" do
+			visit_todo_list(todo_list)
+			within dom_id_for(completed_todo_item) do
+				expect(page).to have_content(completed_todo_item.completed_at)
+			end
+		end
+
+		it "does not give the option to mark complete" do
+			visit_todo_list(todo_list)
+			within dom_id_for(completed_todo_item) do
+				expect(page).to_not have_content("Mark Complete")
+			end
+		end
 	end
 end
